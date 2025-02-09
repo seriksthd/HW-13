0df94343444c457ec4159e02de3444573218cf0d
seriksthd <s29014517@gmail.com>
2025-02-09 13:24:57 +0300

    error

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Routes } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
 import About from "../pages/About";
 import MobaileInfo from "../pages/MobaileInfo";
@@ -6,6 +6,7 @@ import Main from "../components/layout/Main";
 import Products from "../pages/Products";
 import Orders from "../pages/Orders";
 import CartInfo from "../pages/CartInfo";
+import NotFondPage from "../pages/NotFondPage";
 
 export const routes = createBrowserRouter([
   {
@@ -35,4 +36,5 @@ export const routes = createBrowserRouter([
       { path: "/order/:orderId", element: <CartInfo /> },
     ],
   },
+  { path: "*", element: <NotFondPage /> },
 ]);
